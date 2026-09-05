230c24a969be41739b9628bc87952bb247fc77c3
ituaehishenry <2510443@leedstrinity.ac.uk>
2026-09-05 22:41:37 +0100

    Fix Railway port issue

--- app/main.py
+++ app/main.py
@@ -104,4 +104,6 @@ def generate_drift_report():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8002)                            
+    import os
+    port = int(os.getenv("PORT", 8002))
+    uvicorn.run(app, host='0.0.0.0', port=port)
